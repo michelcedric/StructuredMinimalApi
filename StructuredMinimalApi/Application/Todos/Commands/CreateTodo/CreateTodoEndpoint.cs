5a999d0f253b3e66df8ca227c56b29f5207918ce
cedri <cedri@BAS>
2022-12-19 16:09:39 +0100

    Add Cancellation token

--- StructuredMinimalApi/Application/Todos/Commands/CreateTodo/CreateTodoEndpoint.cs
+++ StructuredMinimalApi/Application/Todos/Commands/CreateTodo/CreateTodoEndpoint.cs
@@ -6,11 +6,12 @@ namespace StructuredMinimalApi.Application.Todos.Commands.CreateTodo
     /// <summary>
     /// Endpoint to create a todo
     /// </summary>
-    public class CreateTodoEndpoint : IEndpoint<IResult, CreateTodoCommand>
+    public class CreateTodoEndpoint : IEndpoint<IResult, CreateTodoCommand, CancellationToken>
     {
         public void AddRoute(IEndpointRouteBuilder app)
         {
-            app.MapPost("/Todo", (CreateTodoCommand command) => HandleAsync(command))
+            app.MapPost("/Todo", (CreateTodoCommand command, CancellationToken cancellationToken)
+                => HandleAsync(command, cancellationToken))
                 .Produces<Message>(StatusCodes.Status201Created);
         }
 
@@ -18,8 +19,9 @@ namespace StructuredMinimalApi.Application.Todos.Commands.CreateTodo
         /// Endpoint to create a todo
         /// </summary>
         /// <param name="command"></param>
+        /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public Task<IResult> HandleAsync(CreateTodoCommand command)
+        public Task<IResult> HandleAsync(CreateTodoCommand command, CancellationToken cancellationToken)
         {
             var message = new Message
             {
